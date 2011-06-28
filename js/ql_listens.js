6abccb2d21517adeaac541fe81b837c6af34c4da
Rylan Bowers <rylanb@gmail.com>
2011-06-27 21:49:58 -0600

    Final touches to get this working.

--- js/ql_listens.js
+++ js/ql_listens.js
@@ -1,8 +1,8 @@
 $(function(){
 
   $.ajax({
-    url: "/json/user.json",
-    dataType: "json",
+    url: "http://qlrdio.herokuapp.com/user/collin?callback=?",
+    dataType: "jsonp",
     success: function(data){
       var keyVal = data.key,
           iconPic = data.icon;
@@ -10,24 +10,32 @@ $(function(){
       $('#rdio_wrapper img').attr('src', iconPic);
 
       getHeavyRotation(keyVal);
+      $('#rdio_wrapper').slideDown();
+      $('#loader').addClass('load_2');
     }
   });
 
   function getHeavyRotation(keyVal){
 
     $.ajax({
-      url: "/json/heavyRotation.json",
-      dataType: "json",
+      // url: "/json/heavyRotation.json",
+      url: "http://qlrdio.herokuapp.com/collin/getHeavyRotation",
+      dataType: "jsonp",
       success: function(data){
+        $('#loader').fadeOut(300);
         var items = [];
         $.each(data, function(index, album) {
-          items.push('<li id="' + album.artist + '"><h3>' + album.artist + '</h3><a href="http://rdio.com'+ album.url + '"><img src="' + album.icon + '" alt="' + album.artist + '" /></a></li>');
+          items.push('<li id="' + album.artist + '"><h3>' + album.artist +
+            '</h3><a href="http://rdio.com'+ album.url + '"><img src="' + album.icon + '" alt="' +
+            album.artist + '" /></a><h4>' + album.name + '</h4></li>');
         });
 
         $('<ul/>', {
           'class': 'heavy-rotation-list',
           html: items.join('')
         }).appendTo('#heavy_rotation');
+        $('#music_wrap').delay(400).slideDown(1000);
+
       }
     });
   }
